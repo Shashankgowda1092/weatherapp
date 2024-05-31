448cdd7d382ac85f32dd38a9aca8b496ec75ade3
Yashaswini Sathish <yashaswini_sathish@thbs.india.com>
2024-05-31 17:16:18 +0530

    First version 31st 5 pm

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,6 @@
-// src/components/WeatherCard.jsx
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const WeatherCard = ({ weather }) => {
   if (!weather) {
@@ -8,28 +8,30 @@ const WeatherCard = ({ weather }) => {
   }
 
   return (
-    <Card sx={{ minWidth: 275, margin: 'auto', mt: 2 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {weather.location.name}, {weather.location.country}
-        </Typography>
-        <Typography variant="h6" component="div">
-          {weather.current.condition.text}
-        </Typography>
-        <Typography variant="body2">
-          Temperature: {weather.current.temp_c}°C
-        </Typography>
-        <Typography variant="body2">
-          Humidity: {weather.current.humidity}%
-        </Typography>
-        <Typography variant="body2">
-          Wind: {weather.current.wind_kph} kph
-        </Typography>
-        <Typography variant="body2">
-          Rain: {weather.current.precip_mm} mm
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box sx={{ minWidth: 275, margin: 'auto', mt: 2 }}>
+      <Card variant="outlined" color="transparent" sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {weather.location.name}, {weather.location.country}
+          </Typography>
+          <Typography variant="h6" component="div">
+            {weather.current.condition.text}
+          </Typography>
+          <Typography variant="body2">
+            Temperature: {weather.current.temp_c}°C
+          </Typography>
+          <Typography variant="body2">
+            Humidity: {weather.current.humidity}%
+          </Typography>
+          <Typography variant="body2">
+            Wind: {weather.current.wind_kph} kph
+          </Typography>
+          <Typography variant="body2">
+            Rain: {weather.current.precip_mm} mm
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
